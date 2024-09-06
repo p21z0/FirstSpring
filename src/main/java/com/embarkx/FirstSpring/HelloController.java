284c7be6ac007e271f648732c70db7d8d234f946
p21z0 <pagunuranprim0@gmail.com>
2024-09-06 22:03:49 +0800

    completed controller and response classes

--- src/main/java/com/embarkx/FirstSpring/HelloController.java
+++ src/main/java/com/embarkx/FirstSpring/HelloController.java
@@ -1,6 +1,22 @@
 package com.embarkx.FirstSpring;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello/{name}")
+    public HelloResponse hello(@PathVariable String name) { return new HelloResponse("Hello, "+name);}
+
+    @GetMapping("/hello")
+    public HelloResponse hello(){
+        return new HelloResponse("Hello, World");
+    }
+
+    @PostMapping("/hello")
+    public HelloResponse helloPost(@RequestBody String name){
+        return new HelloResponse("Hello, "+name+"!");
+    }
 }
-//test sdsd
+//test
 //testing again here
